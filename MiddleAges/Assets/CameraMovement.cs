42f42738b724f294ee4864db8ca2be5ead829949
Smirnoff <alexakasmern@tut.by>
2022-10-20 22:47:05 +0300

    feat: add rotation;

--- MiddleAges/Assets/CameraMovement.cs
+++ MiddleAges/Assets/CameraMovement.cs
@@ -2,32 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class CameraMovement : MonoBehaviour
 {
-
     public float moveSpeed = 5f;
 
-    public Rigidbody2D rb;
-    public Camera cam;
-
     public Vector2 movement;
-    public Vector2 mousePos;
+    public Rigidbody2D rb;
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
-        Vector2 lookDir = mousePos - rb.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90f;
-        rb.rotation = angle;
-
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
