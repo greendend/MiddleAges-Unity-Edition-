046c480fe94e3504224f309cc2afb8e8ef4faaa6
Smirnoff <alexakasmern@tut.by>
2022-10-22 23:06:47 +0300

    feat: camera movement + keys movement;

--- MiddleAges/Assets/CameraMovement.cs
+++ MiddleAges/Assets/CameraMovement.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-
-    public Vector2 movement;
     public Rigidbody2D rb;
-
-    // Update is called once per frame
-    void Update()
-    {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-    }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.position = GameObject.FindGameObjectWithTag("Player_01").transform.position;
     }
 }
