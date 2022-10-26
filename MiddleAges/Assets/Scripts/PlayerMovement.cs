e6265f62d19478fd04001de12638ce29582a9bea
Smirnoff <alexakasmern@tut.by>
2022-10-26 22:02:01 +0300

    feat: initial network;

--- MiddleAges/Assets/Scripts/PlayerMovement.cs
+++ MiddleAges/Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : NetworkBehaviour
 {
     public float moveSpeed = 5f;
 
@@ -10,49 +11,55 @@ public class PlayerMovement : MonoBehaviour
     public Camera cam;
 
     public Vector2 mousePos;
-    public Vector2 posRight;
+    public Vector2 lookDirRight;
+
+    void Start()
+    {
+        if (hasAuthority)
+        {
+            cam = new Camera();
+        }
+    }
 
     // Update is called once per frame
     void Update()
-    {        
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        posRight = rb.transform.right;
+    {
+        if (hasAuthority)
+        {
+            mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+            lookDirRight = rb.transform.right;
+        }
     }
 
     private void FixedUpdate()
-    {        
-        Vector2 lookDir = mousePos - rb.position;
-        Vector2 lookDirRight = posRight;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90f;
-        rb.rotation = angle;
-
-        lookDir.Normalize();
-        lookDirRight.Normalize();
-
-        if (Input.GetKey(KeyCode.W))
+    {
+        if (hasAuthority)
         {
-            rb.MovePosition(rb.position + lookDir * moveSpeed * Time.fixedDeltaTime);
-        } 
-        
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.MovePosition(rb.position - lookDir * moveSpeed * Time.fixedDeltaTime);
+            Vector2 lookDir = mousePos - rb.position;
+            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90f;
+            rb.rotation = angle;
+
+            lookDir.Normalize();
+            //lookDirRight.Normalize();
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.MovePosition(rb.position + lookDir * moveSpeed * Time.fixedDeltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb.MovePosition(rb.position - lookDir * moveSpeed * Time.fixedDeltaTime);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.MovePosition(rb.position - lookDirRight * moveSpeed * Time.fixedDeltaTime);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.MovePosition(rb.position + lookDirRight * moveSpeed * Time.fixedDeltaTime);
+            }
         }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.MovePosition(rb.position - lookDirRight * moveSpeed * Time.fixedDeltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.MovePosition(rb.position + lookDirRight * moveSpeed * Time.fixedDeltaTime);
-        }
-
-        //else if (Input.GetKey(KeyCode.A))
-        //{
-        //lookDir.x = -(lookDir.y) * Mathf.Sin(90f * Mathf.Rad2Deg) + (lookDir.x) * Mathf.Cos(90f * Mathf.Rad2Deg);
-        //lookDir.y = (lookDir.y) * Mathf.Cos(90f * Mathf.Rad2Deg) + (lookDir.x) * Mathf.Sin(90f * Mathf.Rad2Deg);
-        //rb.MovePosition(rb.position - lookDir * moveSpeed * Time.fixedDeltaTime);
-        //}
-
     }
 }
